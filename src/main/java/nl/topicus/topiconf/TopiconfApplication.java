b3e73ca1d8cf11a25b67cfa3bfecc5011ccd1186
Raymond Loman <r.loman@recognize.nl>
2023-02-21 11:21:08 +0100

    Initial load

---
+++ src/main/java/nl/topicus/topiconf/TopiconfApplication.java
@@ -0,0 +1,13 @@
+package nl.topicus.topiconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopiconfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopiconfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/topicus/topiconf/TopiconfApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.topicus.topiconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopiconfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
